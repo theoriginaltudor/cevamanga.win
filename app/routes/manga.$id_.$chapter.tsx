1893d924e33427de8b8d3c8ce6e168bacfd334cd
Tudor Caseru <theoriginaltudor@gmail.com>
2024-10-27 10:58:26 +0100

    add next and prev buttons

--- app/routes/manga.$id_.$chapter.tsx
+++ app/routes/manga.$id_.$chapter.tsx
@@ -1,5 +1,5 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { mangaChapter } from "~/types/mangaChapter";
 import { domain } from "./manga._index";
 
@@ -7,17 +7,56 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   const apiResponse = await fetch(
     new URL(`${domain}/api/manga/${params.id}/${params.chapter}`)
   );
+  const [prefix, currentChapter] = params.chapter?.split("-") || ["", ""];
+  const controlData: Props = {
+    manga: params.id || "",
+    prev: `${prefix}-${parseInt(currentChapter) - 1}` || "",
+    next: `${prefix}-${parseInt(currentChapter) + 1}` || "",
+  };
+
   const objectResponse: mangaChapter = await apiResponse.json();
-  return json({ ...objectResponse });
+  return json({
+    ...objectResponse,
+    controlData,
+  });
 };
 
 export default function MangaChapter() {
-  const { images } = useLoaderData<typeof loader>();
+  const { images, controlData } = useLoaderData<typeof loader>();
   return (
     <>
+      <PrevNext {...controlData} />
       {images.map((page) => (
         <img src={page.image} className="w-full" />
       ))}
+      <PrevNext {...controlData} />
     </>
   );
 }
+
+type Props = {
+  prev: string;
+  next: string;
+  manga: string;
+};
+
+const PrevNext = ({ prev, next, manga }: Props) => {
+  return (
+    <div className="bg-black text-xl text-white flex justify-center gap-10">
+      <Link
+        to={`/manga/${manga}/${prev}`}
+        className="border-white border p-3"
+        prefetch="intent"
+      >
+        {"<<"} Previous
+      </Link>
+      <Link
+        to={`/manga/${manga}/${next}`}
+        className="border-white border p-3"
+        prefetch="intent"
+      >
+        Next {">>"}
+      </Link>
+    </div>
+  );
+};
